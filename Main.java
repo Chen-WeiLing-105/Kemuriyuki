eb06531145d157c2ebac3ce0cad54f311c84c4b5
Chen-WeiLing-105 <43446312+Chen-WeiLing-105@users.noreply.github.com>
2018-09-20 23:11:05 +0800

    HW1-3

--- Main.java
+++ Main.java
@@ -1,38 +1,40 @@
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        int v1,v2,v3,v4,v5;
-        Scanner scn= new Scanner(System.in) ;
-        System.out.println("輸入 整數a:");
-        v1 = scn.nextInt();
-        System.out.println("輸入 整數b:");
-        v2= scn.nextInt();
+        Scanner nmb=new Scanner(System.in);
+        int v1 , v2 ,v3 ;
+        System.out.println("請輸入國文成績:");
+        v1 = nmb.nextInt();
+        //System.out.println("請輸入英文成績:");
+        //v2 = nmb.nextInt();
+        //System.out.println("請輸入數學成績:");
+        //v3 =nmb.nextInt();
 
-        v3=v1+v2;
-        v4=v1-v2;
-        v5=v1*v2;
+        if( 0<=v1 && v1<=100  ){
+            System.out.println("請輸入英文成績:");
+            v2 = nmb.nextInt();
 
-        System.out.println(v3);
-        System.out.println(v4);
-        System.out.println(v5);
+            if(v2<=100 && v2 >= 0){
 
-        // write your code here
+                System.out.println("請輸入數學成績:");
+                v3 =nmb.nextInt();
+
+                if(v3<=100 && v3>=0){
+                    System.out.println(" 總成績 : " + (v1 + v2 + v3));
+                    System.out.println(" 平均成績 : " + (v1 + v2 + v3) / 3);
+
+                }else { System.out.println("數學成績輸入錯誤!"); }
+
+            }else {System.out.println("英文成績輸入錯誤!"); }
+
+        }else {System.out.print("國文成績輸入錯誤!"); }
+
+
+        }
+
+
+	// write your code here
     }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
