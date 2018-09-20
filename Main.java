52a64c2c22b90f5222faf4d04cdbb39225e919c8
Chen-WeiLing-105 <43446312+Chen-WeiLing-105@users.noreply.github.com>
2018-09-20 23:02:53 +0800

    HW1-1

--- Main.java
+++ Main.java
@@ -1,38 +1,19 @@
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        int v1,v2,v3,v4,v5;
-        Scanner scn= new Scanner(System.in) ;
-        System.out.println("輸入 整數a:");
-        v1 = scn.nextInt();
-        System.out.println("輸入 整數b:");
-        v2= scn.nextInt();
+        System.out.println("輸入攝氏溫度:");
+        Scanner scn=new Scanner(System.in);
+        int v1 =scn .nextInt( );
+        int v2;
+        v2=v1* 9/5+32;
 
-        v3=v1+v2;
-        v4=v1-v2;
-        v5=v1*v2;
+        System.out.println( "華氏溫度:"+v2 );
 
-        System.out.println(v3);
-        System.out.println(v4);
-        System.out.println(v5);
 
-        // write your code here
+
+
+	// write your code here
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
